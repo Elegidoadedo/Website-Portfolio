9ebdd66aa6888c0b36e8ceb0369d14d6d37dae12
Jose Morales <jmoralesmnz@gmail.com>
2024-10-03 18:38:42 +0200

    Added Medium link to social Media

--- astro-2024/src/content/socialMediaUrl.ts
+++ astro-2024/src/content/socialMediaUrl.ts
@@ -4,6 +4,8 @@ const socialMediaUrl = {
   thingiverse: "https://www.thingiverse.com/elegidoadedo/designs",
   instagram: "https://www.instagram.com/creativoadedo",
   mail: "mailto:jmoralesmnz@gmail.com",
+  medium: "https://elegidoadedo.medium.com",
+  ivoox: "https://go.ivoox.com/sq/2473950"
 };
 
 export default socialMediaUrl;
